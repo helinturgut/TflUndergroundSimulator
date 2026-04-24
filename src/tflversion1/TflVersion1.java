0516fbc128ad890c743669e86309478e24753ba0
Helin Turgut <helinturgutn@hotmail.com>
2026-04-24 17:40:35 +0300

    Add: initial project files

--- src/tflversion1/TflVersion1.java
+++ src/tflversion1/TflVersion1.java
@@ -1,20 +1,14 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package tflversion1;
 
 /**
  *
- * @author PC
+ * @author w20991801
  */
 public class TflVersion1 {
 
-    /**
-     * @param args the command line arguments
-     */
+   
     public static void main(String[] args) {
-        // TODO code application logic here
+      
     }
     
 }
